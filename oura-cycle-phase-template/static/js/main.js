f3b4de4dbf4aea2a0f613fb60ba32035d1ed70e0
Diogo Ribeiro <68607977+SGT-RIBEIRO@users.noreply.github.com>
2023-04-08 09:51:19 -0400

    Add files via upload

---
+++ oura-cycle-phase-template/static/js/main.js
@@ -0,0 +1,29 @@
+const messageForm = document.querySelector('form');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+messageForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const payload = {
+        username: emailInput.value,
+        password: passwordInput.value
+    }
+
+    console.log(JSON.stringify(payload));
+
+    const response = await fetch('http://13.59.99.183:3000/login', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(payload),
+        mode: 'cors'
+    });
+
+    if (response.status === 200) {
+        const data = await response.json();
+        alert(
+            "Login realizado com sucesso!"
+        );
+    }
+});
+
+
+
