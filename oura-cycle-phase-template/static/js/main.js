d20ff4088df170f8c42a0d74b4e0225f4a3c62b7
Diogo Ribeiro <68607977+SGT-RIBEIRO@users.noreply.github.com>
2023-04-08 14:16:38 -0400

    Update main.js

--- oura-cycle-phase-template/static/js/main.js
+++ oura-cycle-phase-template/static/js/main.js
@@ -14,7 +14,7 @@ messageForm.addEventListener('submit', async (event) => {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(payload),
-        mode: 'no-cors',
+        mode: 'cors',
         referrerPolicy: 'unsafe-url'
     });
 
